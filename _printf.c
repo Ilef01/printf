07c3494a1b1cedd3df1440c00966966c272596e9
josfam <josephamukun@tutanota.com>
2023-10-15 11:49:11 +0300

    Add starter files
    `main.h`, which contains function prototypes, and
    `_printf.c`, which will contain the actual implementation of
    `_printf` have been added.

---
+++ _printf.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _printf - Prints a formatted string to the standard output stream.
+ * @format: The character string, that will contain zero or more directives.
+ * Description: Prints a formatted string to the standard output stream.
+ * Return: The number of characters printed (excluding the null byte used to
+ *         end output to strings)
+ */
+int _printf(const char *format, ...)
+{
+	/* TODO */
+	return (0);
+}
+
---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+/* Prints a formatted string to the screen. */
+int _printf(const char *format, ...);
+
+#endif
