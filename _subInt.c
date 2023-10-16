8668dfe3e758b97028ddaef22b726bdf01eafb69
Ilef01 <bchinyyilef@gmail.com>
2023-10-16 11:25:30 -0700

    The solution is now recursive

--- _subInt.c
+++ _subInt.c
@@ -12,9 +12,6 @@
 void _subInt(va_list allargs, int *charCount)
 {
 	int num = va_arg(allargs, int); /* get the number */
-	int digit; /* each digit composing the number */
-	char chardigit; /* the digit converted to char */
-	int div = 1; /* divisor */
 
 	/* handle the case for negative numbers */
 	if (num < 0)
@@ -23,14 +20,7 @@ void _subInt(va_list allargs, int *charCount)
 		(*charCount)++;
 		num = -num; /* remove the minus */
 	}
-	while (num / div >= 10)
-		div *= 10;
-	while (div >= 1)
-	{
-		digit = (num / div) % 10; /* get the largest dogit */
-		chardigit = digit + '0'; /* the conversion from int to char */
-		_putchar (chardigit);
-		(*charCount)++;
-		div /= 10;
-	}
+	if (num / 10 != 0)
+		_subInt(num / 10);
+	_putchar ('0' + num % 10);
 }
