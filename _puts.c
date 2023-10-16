ba7a8a6afb7f9226a055e3a7208d65d05c5aef83
josfam <josephamukun@tutanota.com>
2023-10-16 21:01:55 +0300

    Return ints from `_puts` and `_subStr`
    For error-handling, `_puts` and `_subStr` now return integers
    instead of void.
    The returned value can now be checked to determine if anything
    went wrong.
    Returning ints is also consistent with the standard library's own
    signatures.

--- _puts.c
+++ _puts.c
@@ -1,19 +1,25 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _puts - Prints a string, followed by a new line.
  * @str: The string to print.
  * Description: Prints a string to stdout.
  *              Does not print a new line at the end of the string.
- * Return: Nothing.
+ * Return: A non-negative int on success, -1 on error.
  */
-void _puts(char *str)
+int _puts(char *str)
 {
-	int i;
-	int len = _strlen(str);
+	int i, len;
+
+	if (str == NULL)
+		return (-1);
+
+	len = _strlen(str);
 
 	for (i = 0; i < len; i++)
 	{
 		_putchar(str[i]);
 	}
+	return (0);
 }
